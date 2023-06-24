f654bd14a68b141c8d09d956f61b3c40edc09f05
Dawid Kaluża <dawid.kaluza@tensquaregames.com>
2023-06-24 22:22:34 +0200

    More WebGL fixes

--- Assets/Scripts/Gameplay/Quests/WatchVideoQuest.cs
+++ Assets/Scripts/Gameplay/Quests/WatchVideoQuest.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using DG.Tweening.Plugins.Core.PathCore;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Video;
@@ -11,8 +12,8 @@ namespace Yarde.Gameplay.Quests
     [CreateAssetMenu(fileName = "WatchVideoQuest", menuName = "Quests/WatchVideoQuest", order = 3)]
     public class WatchVideoQuest : Quest
     {
-        [SerializeField] private VideoClip _videoClip;
-        [SerializeField] private string _videoUrl;
+        [SerializeField] private string _videoName;
+        [SerializeField] private float _videoLength;
         [Inject] [UsedImplicitly] private VideoPlayer _videoPlayer;
 
         protected override void RunInternal()
@@ -21,14 +22,12 @@ namespace Yarde.Gameplay.Quests
 
         protected override async UniTask SuccessCondition(CancellationTokenSource cts)
         {
-            _videoPlayer.source = Application.platform == RuntimePlatform.WebGLPlayer
-                ? VideoSource.Url
-                : VideoSource.VideoClip;
             _videoPlayer.gameObject.SetActive(true);
-            _videoPlayer.clip = _videoClip;
-            _videoPlayer.url = _videoUrl;
-            var length = (int)(_videoClip.length * 1000);
-            Debug.Log($"Playing video {_videoClip.name}, length: {length} ms");
+            var videoUrl = System.IO.Path.Combine(Application.streamingAssetsPath, _videoName);
+            _videoPlayer.url = videoUrl;
+            _videoPlayer.Prepare();
+            var length = (int)(_videoLength * 1000);
+            Debug.Log($"Playing video {_videoName}, length: {length} ms");
             _videoPlayer.Play();
             await UniTask.Delay(length, cancellationToken: cts.Token);
             _videoPlayer.gameObject.SetActive(false);
